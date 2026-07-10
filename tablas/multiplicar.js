71bf1154065ea2330f983f8683f68f2c4baacdd8
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-10 18:55:33 -0500

    Agregando función generarTablas y conectar al botón

---
+++ tablas/multiplicar.js
@@ -0,0 +1,8 @@
+function generarTablas(){
+    let contenido = "";
+    let contenedor = document.getElementById("tabla");
+    let numero = parseInt(document.getElementById("txtNumero").value);
+ 
+    contenedor.innerHTML = contenido;
+}
+ 
