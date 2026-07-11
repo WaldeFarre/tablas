f576c52ab9754470a61ad1d61585ca21e5a3ba6a
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-10 19:04:34 -0500

    Agregando instrucciones y mensaje de help

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,12 +1,17 @@
+
+Multiplicar · JS
 function generarTablas(){
     let contenido = "";
     let contenedor = document.getElementById("tabla");
     let numero = parseInt(document.getElementById("txtNumero").value);
  
-for(let i = 1; i <= 12; i++){
-        contenido = contenido + "<div class='fila'>" + numero + " × " + i + " = " + (numero * i) + "</div>";
-}
-    contenedor.innerHTML = contenido;
-
-}
+    if(isNaN(numero)){
+        contenedor.innerHTML = "<p class='aviso'>Escribe un número para ver su tabla ✨</p>";
+        return;
+    }
  
+    for(let i = 1; i <= 12; i++){
+        contenido = contenido + "<div class='fila'>" + numero + " × " + i + " = " + (numero * i) + "</div>";
+    }
+    contenedor.innerHTML = contenido;
+}
