ef9d24c0aa2300b1f761e9124821b23fb65957ed
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-10 19:07:00 -0500

    Correción final y lista para vercel

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,15 +1,13 @@
-
-Multiplicar · JS
 function generarTablas(){
     let contenido = "";
     let contenedor = document.getElementById("tabla");
     let numero = parseInt(document.getElementById("txtNumero").value);
- 
+
     if(isNaN(numero)){
         contenedor.innerHTML = "<p class='aviso'>Escribe un número para ver su tabla ✨</p>";
         return;
     }
- 
+
     for(let i = 1; i <= 12; i++){
         contenido = contenido + "<div class='fila'>" + numero + " × " + i + " = " + (numero * i) + "</div>";
     }
